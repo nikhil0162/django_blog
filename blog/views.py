3d2b7c677fc648508e16bd64db26ea16961bc370
nikhil0162 <nikhil0162@gmail.com>
2019-01-27 17:55:38 +0530

    game_programmer

--- blog/views.py
+++ blog/views.py
@@ -69,4 +69,7 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 
 
 def about(request):
-	return render(request,'blog/about.html', {'title':'About'})	
+	return render(request,'blog/about.html', {'title':'About'})
+
+def game_program():
+	pass
